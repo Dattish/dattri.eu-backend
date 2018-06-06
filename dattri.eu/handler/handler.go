49a08904aaff44043be622918e7cd9a4c4cd9cb8
Dat Trieu <qdattrieu@gmail.com>
2018-06-06 14:11:27 +0200

    allow the server to act as a reverse proxy

--- dattri.eu/handler/handler.go
+++ dattri.eu/handler/handler.go
@@ -1,17 +1,13 @@
 package handler
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
 )
-
-type endpoint struct {
-	Endpoint     string `json:"endpoint"`
-	ResourceType string `json:"resourceType"`
-	Path         string `json:"path"`
-}
 
 type endpointError struct {
 	error string
@@ -39,24 +35,72 @@ func directoryHandler(serveMux *http.ServeMux, endpoint string, path string) err
 	return nil
 }
 
+func proxyHandler(serveMux *http.ServeMux, endpoint string, method string, path string) {
+	//TODO: make this testable, how to properly inject client?
+	serveMux.HandleFunc(endpoint, func(w http.ResponseWriter, r *http.Request) {
+		var body []byte
+		if r.Body != nil {
+			b, err := ioutil.ReadAll(r.Body)
+			defer r.Body.Close()
+			if err != nil {
+				http.Error(w, err.Error(), 500)
+				return
+			}
+			body = b
+		}
+
+		queryParams := r.URL.RawQuery
+		if queryParams != ""{
+			queryParams = "?" + queryParams
+		}
+
+		request, err := http.NewRequest(method, path + queryParams, bytes.NewBuffer(body))
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+
+		client := &http.Client{}
+		response, err := client.Do(request)
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+		responseBody := &bytes.Buffer{}
+		if response.Body != nil {
+			_, err = responseBody.ReadFrom(response.Body)
+			if err != nil {
+				http.Error(w, err.Error(), 500)
+				return
+			}
+			defer response.Body.Close()
+		}
+
+		w.Header().Set("Content-Type", response.Header.Get("Content-Type"))
+		w.Write(responseBody.Bytes())
+	})
+}
+
 //Sets up endpoints for files and directories based on a json in []byte format
 //Note that directory endpoints needs to begin and end with a slash, i.e "/test/"
 func EndpointsFromConfig(serveMux *http.ServeMux, rawJson []byte) error {
-	var endpoints []endpoint
+	var endpoints []map[string]string
 	json.Unmarshal(rawJson, &endpoints)
 
 	for _, endpoint := range endpoints {
-		switch resourceType := endpoint.ResourceType; resourceType {
+		switch resourceType := endpoint["resourceType"]; resourceType {
 		case "file":
-			err := fileHandler(serveMux, endpoint.Endpoint, endpoint.Path)
+			err := fileHandler(serveMux, endpoint["endpoint"], endpoint["path"])
 			if err != nil {
 				return err
 			}
 		case "directory":
-			err := directoryHandler(serveMux, endpoint.Endpoint, endpoint.Path)
+			err := directoryHandler(serveMux, endpoint["endpoint"], endpoint["path"])
 			if err != nil {
 				return err
 			}
+		case "proxy":
+			proxyHandler(serveMux, endpoint["endpoint"], endpoint["method"], endpoint["path"])
 		default:
 			return &endpointError{fmt.Sprintf("No such resourceType: %s", resourceType)}
 		}
